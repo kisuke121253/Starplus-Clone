6d1c0f0a24346566daf30af6954f69ba2146f5fd
kisuke121253 <jpedro121253@gmail.com>
2023-02-22 20:34:17 -0300

    #

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const nav1 = document.getElementById('nav1')
+const nav2 = document.getElementById('nav2')
+const hd_1 = document.getElementById('hd_1')
+const btnS_2 = document.getElementById('btnS_2')
+
+
+
+
